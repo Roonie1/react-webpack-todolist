34c9f6bea9ed534e7168fecee29929319e62899b
wade <ronnie@wangliuzhandeMacBook-Pro.local>
2015-11-23 09:56:03 +0800

    处理由于react与reactDOM分离出现的警告
    处理由于react与reactDOM分离出现的警告

--- app/main.js
+++ app/main.js
@@ -2,7 +2,8 @@
  * Created by ronnie on 15/11/20.
  */
 import React from "react";
+import ReactDOM from "react-dom";
 import ReactBootstrap from "react-bootstrap";
 import Container from "./Container.jsx";
 
-React.render(<Container/>,document.getElementById('content'));
+ReactDOM.render(<Container/>,document.getElementById('content'));
